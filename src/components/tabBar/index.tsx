ea16ec0cb38edbfcf7a28203f76c459d39de03b5
zhuxuechang <1494135711@qq.com>
2023-01-17 17:44:55 +0800

    feat: disable animation when click

--- src/components/tabBar/index.tsx
+++ src/components/tabBar/index.tsx
@@ -19,6 +19,7 @@ export default function TabBar() {
         tabBarIndicatorStyle: {
           height: 0,
         },
+        animationEnabled: false,
         tabBarBounces: false,
         tabBarPressColor: '#ccc',
       }}
